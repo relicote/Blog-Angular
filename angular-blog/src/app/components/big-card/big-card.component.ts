df48a182894b8a1d85c7ced89a907c6b0ead9d7a
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-09-04 20:14:34 -0300

    att big-card

--- angular-blog/src/app/components/big-card/big-card.component.ts
+++ angular-blog/src/app/components/big-card/big-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./big-card.component.css']
 })
 export class BigCardComponent implements OnInit {
+
+    @Input()
+    photoCover: String = '';
+    @Input()
+    cardTitle: String = 'NOVO JOESTAR ANUNCIADO (SPOILER)';
+    @Input()
+    cardDescription: string = 'O novo Jojo já tem nome e aparencia, venha conferir em primeira mão!'
 
   constructor() { }
 
