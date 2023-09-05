ed57cb80be8769dad7827879d1062b46a7511d7c
Gabriel Reliquias <125402939+relicote@users.noreply.github.com>
2023-09-05 12:26:42 -0300

    att content.component

--- angular-blog/src/app/pages/content/content.component.ts
+++ angular-blog/src/app/pages/content/content.component.ts
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContentComponent implements OnInit {
 
+  photoCover: string = 'https://www.siliconera.com/wp-content/uploads/2023/02/jjbajojoseoparts02132023.png?fit=710%2C398'
+  contentTitle: string = 'MINHA NOTICIA'
+  contentDescription: string = 'bla bla bla'
+
   constructor() { }
 
   ngOnInit(): void {
